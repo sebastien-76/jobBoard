2f57fed64586d762abf952ead273518144169a83
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-10-03 09:47:20 +0200

    affichage des offres

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Offre;
+use App\Repository\OffreRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class JobController extends AbstractController
 {
     #[Route('/', name: 'app_job')]
-    public function index(): Response
+    public function index(OffreRepository $offreRepository): Response
     {
         return $this->render('job/index.html.twig', [
-            'controller_name' => 'JobController',
+            'offres' => $offreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_job_show')]
+    public function show(Offre $offre): Response
+    {
+        return $this->render('job/show.html.twig', [
+            'offre' => $offre,
         ]);
     }
 }
